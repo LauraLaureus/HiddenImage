41297e6dae422725f0ddd23d26c68acd64f385a1
Laura <lauradelpinodiaz94@gmail.com>
2017-02-04 23:57:41 +0100

    dark images

--- extractimage.m
+++ extractimage.m
@@ -1,6 +1,8 @@
 Original = imread('castillo_original.jpeg');
 Cover = imread('Castillo_oculto2bits.jpeg');
 
+
+
 %imagen de tres escalas de grises
 threegreyscale = bitshift(Cover,6);
 figure,imshow(threegreyscale)
@@ -11,6 +13,7 @@ figure,imshow(threegreyscale)
 %imagen de dos escalas de grises
 twogreyscale = bitshift(Cover,7);
 figure,imshow(twogreyscale)
+
 
 twogreyscale = 2.*twogreyscale;
 figure,imshow(twogreyscale)
