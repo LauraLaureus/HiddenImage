af227df07e2092d02ba6684afc957559c410e6c9
Laura <lauradelpinodiaz94@gmail.com>
2017-02-04 23:05:14 +0100

    images and script

---
+++ extractimage.m
@@ -0,0 +1,17 @@
+Original = imread('castillo_original.jpeg');
+Cover = imread('Castillo_oculto2bits.jpeg');
+
+%imagen de tres escalas de grises
+threegreyscale = bitshift(Cover,6);
+figure,imshow(threegreyscale)
+
+threegreyscale = 2.*threegreyscale;
+figure,imshow(threegreyscale)
+
+%imagen de dos escalas de grises
+twogreyscale = bitshift(Cover,7);
+figure,imshow(twogreyscale)
+
+twogreyscale = 2.*twogreyscale;
+figure,imshow(twogreyscale)
+
